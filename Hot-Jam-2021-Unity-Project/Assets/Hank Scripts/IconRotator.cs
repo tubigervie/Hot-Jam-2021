145723793fdb660e96a9e9f72e815d18563d3bb8
Hank <shiawhern@gmail.com>
2021-03-26 20:01:22 -0700

    Updated HanV4 and StartMenu

---
+++ Hot-Jam-2021-Unity-Project/Assets/Hank Scripts/IconRotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconRotator : MonoBehaviour
+{
+    [SerializeField] float magnitude = 1f;
+    [SerializeField] float frequency = 1f;
+    [SerializeField] float degreeOffset = 0f;
+
+    RectTransform rectTrans;
+
+    Vector3 rotation = Vector3.zero;
+
+    float timer = 0f;
+
+    private void Start()
+    {
+        rectTrans = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        rectTrans.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Sin(timer * frequency) * magnitude + degreeOffset));
+    }
+}
